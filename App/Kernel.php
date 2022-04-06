fe724b6742964f0bf98e7d30d552a06da0a3eb34
JirakLu <jirak.lukas@gmail.com>
2022-04-06 08:53:32 +0200

    Moved register composers from AController to Kernel.

--- App/Kernel.php
+++ App/Kernel.php
@@ -7,11 +7,60 @@ use Services\Router;
 
 class Kernel {
 
+    /** @var array<String> */
+    private array $viewComposers = [];
+    private Blade $blade;
+
+    public function __construct()
+    {
+        foreach (glob(__DIR__ . "/Views/*") as $class) {
+            $className = explode("/", $class);
+            $className = array_pop($className);
+            $className = explode(".", $className);
+            $className = array_shift($className);
+            $this->viewComposers[$class] = strtolower($className);
+        }
+    }
+
+    private function registerComposers(string $viewsDir): void
+    {
+        foreach (glob($viewsDir."/*") as $file) {
+            if (is_dir($file)) {
+                $this->registerComposers($file);
+            } else {
+                $fileName = explode("/", "$file");
+                $fileName = array_pop($fileName);
+                $fileName = explode(".", $fileName);
+                $fileName = array_shift($fileName);
+                $fileName = strtolower($fileName . "composer");
+
+
+                if (in_array($fileName, $this->viewComposers)) {
+                    $classPath = array_search($fileName, $this->viewComposers);
+
+                    $file = explode("views/", $file);
+                    $file = array_pop($file);
+                    $file = str_replace("/", ".", $file);
+                    $file = str_replace(".blade.php", "", $file);
+
+                    $classPath = explode("Views/", $classPath);
+                    $classPath = array_pop($classPath);
+                    $classPath = str_replace(".php", "", $classPath);
+
+                    $this->blade->composer($file, "App\Views\\$classPath");
+
+                }
+            }
+        }
+    }
+
     public function listen(): void
     {
         mb_internal_encoding('UTF-8');
 
-        $blade = new Blade(__DIR__."/../resources/views", __DIR__."/../cache");
+        $this->blade = new Blade(__DIR__."/../resources/views", __DIR__."/../cache");
+
+        $this->registerComposers(__DIR__ . "/../resources/views");
 
         Session::start();
 
@@ -19,6 +68,6 @@ class Kernel {
         $action = $router->findRoute();
 
 
-        (new $action["controller"]($router, $blade))->{$action["action"]}(array_key_exists("params",$action) ? $action["params"] : null);
+        (new $action["controller"]($router, $this->blade))->{$action["action"]}(array_key_exists("params",$action) ? $action["params"] : null);
     }
 }
